306f0adb69c63a10356dd4a5d91a5829676715d3
Dante Issaias <dante@issaias.com>
2024-12-04 15:32:46 +0000

    Fix blink icons

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,16 +3,15 @@ return {
   event = "InsertEnter",
   build = "cargo +nightly build --release",
   opts = {
+    nerd_font_variant = "mono",
     keymap = {
       preset = "enter",
     },
     sources = {
-      -- add lazydev to your completion providers
       completion = {
         enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev" },
       },
       providers = {
-        -- dont show LuaLS require statements when lazydev has items
         lsp = { fallback_for = { "lazydev" } },
         lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
       },
