a40ac29f20ccb8b6b9091d18f481a6315cbe0bd8
Dante Issaias <dante@issaias.com>
2024-12-04 15:33:01 +0000

    Skip confirm for simple edits

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,7 +4,11 @@ return {
     { "-", "<cmd>Oil<cr>" },
     { "_", "<cmd>Oil .<cr>" },
   },
+  ---@module "oil"
+  ---@type oil.SetupOpts
   opts = {
+    default_file_explorer = true,
+    skip_confirm_for_simple_edits = true,
     view_options = {
       show_hidden = true,
       is_always_hidden = function(name)
