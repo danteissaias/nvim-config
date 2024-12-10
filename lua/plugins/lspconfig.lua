727f224d7765d5e0d13f1616b043d6584aafa4a1
Dante Issaias <dante@issaias.com>
2024-12-10 23:44:55 +0000

    Improve typescript lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -23,17 +23,23 @@ return {
       vtsls = {
         settings = {
           typescript = {
+            preferences = {
+              preferTypeOnlyAutoImports = true,
+            },
             updateImportsOnFileMove = "always",
+            workspaceSymbols = {
+              scope = "currentProject",
+              excludeLibrarySymbols = true,
+            },
           },
-          javascript = {
-            updateImportsOnFileMove = "always",
-          },
+          javascript = { updateImportsOnFileMove = "always" },
           vtsls = {
             enableMoveToFileCodeAction = true,
             autoUseWorkspaceTsdk = true,
             experimental = {
               completion = {
                 enableServerSideFuzzyMatch = true,
+                entriesLimit = 75,
               },
             },
             typescript = {
