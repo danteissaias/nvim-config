00ba3e331404c63c2e4f602f87b3da1e850f5834
Dante Issaias <dante@issaias.com>
2024-12-12 18:57:37 +0000

    Update blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -5,14 +5,23 @@ return {
   opts = {
     appearance = { nerd_font_variant = "mono" },
     completion = {
+      trigger = {
+        prefetch = true,
+      },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 0,
         update_delay_ms = 0,
       },
+      accept = {
+        auto_brackets = {
+          enabled = false,
+        },
+      },
     },
     sources = {
       default = { "lsp", "path", "snippets", "buffer", "lazydev" },
+      cmdline = {},
       providers = {
         lsp = { fallback = { "lazydev" } },
         lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
