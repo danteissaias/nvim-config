26024ca7b4042c6f9135bb29a11c5438389b3931
Dante Issaias <dante@issaias.com>
2024-12-14 21:41:17 +0000

    Update plugins

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,13 +1,17 @@
+---@diagnostic disable: missing-fields
+
 return {
   "Saghen/blink.cmp",
   lazy = false,
   build = "cargo +nightly build --release",
+  ---@module "blink.cmp"
+  ---@type blink.cmp.Config
   opts = {
     appearance = { nerd_font_variant = "mono" },
     completion = {
-      trigger = {
-        prefetch_on_insert = true,
-      },
+      -- trigger = {
+      --   prefetch_on_insert = true,
+      -- },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 50,
@@ -17,9 +21,6 @@ return {
         auto_brackets = {
           enabled = false,
         },
-      },
-      menu = {
-        scrollbar = true,
       },
     },
     sources = {
