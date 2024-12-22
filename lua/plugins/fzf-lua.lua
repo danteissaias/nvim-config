b3e816e35541c9f32176d1ca6b1ec51dd4fbbb42
Dante Issaias <dante@issaias.com>
2024-12-22 19:32:28 +0000

    Use native live grep

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -2,7 +2,7 @@ return {
   "ibhagwan/fzf-lua",
   keys = {
     { "<leader>sf", "<cmd>Fzf files<cr>" },
-    { "<leader>sg", "<cmd>Fzf live_grep<cr>" },
+    { "<leader>sg", "<cmd>Fzf live_grep_native<cr>" },
   },
   config = function()
     require("fzf-lua").setup { "max-perf" }
