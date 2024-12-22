5bc593f1c5b6f8a4af2b9d7a48225e375ef99be4
Dante Issaias <dante@issaias.com>
2024-12-22 19:32:41 +0000

    Keymaps for quickfix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,6 +10,9 @@ map("n", "<c-h>", "<c-w><c-h>")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
