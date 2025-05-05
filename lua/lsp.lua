21c9061d14ef111cde82f0d2077cf73ab7a2f16f
Dante Issaias <dante@issaias.com>
2025-05-05 12:25:36 +0100

    Add lsp error keymaps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,3 +1,32 @@
+---@param client vim.lsp.Client
+---@param bufnr integer
+local function on_attach(client, bufnr)
+  ---@param lhs string
+  ---@param rhs string|function
+  ---@param desc string
+  ---@param mode? string|string[]
+  local function keymap(lhs, rhs, desc, mode)
+    mode = mode or "n"
+    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+  end
+
+  keymap("[e", function()
+    vim.diagnostic.jump { count = -1, severity = vim.diagnostic.severity.ERROR }
+  end, "Previous error")
+  keymap("]e", function()
+    vim.diagnostic.jump { count = 1, severity = vim.diagnostic.severity.ERROR }
+  end, "Next error")
+
+  -- vim.lsp.document_color.enable(true, bufnr)
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    on_attach(client, args.buf)
+  end,
+})
+
 -- Set up LSP servers.
 vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
   once = true,
