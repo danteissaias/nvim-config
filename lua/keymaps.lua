fb0dacfa968d5ca40d445bb4a2c1f1f14817137e
Dante Issaias <dante@issaias.com>
2025-06-03 13:50:06 +0100

    Fix

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -7,8 +7,8 @@ map("n", "<c-k>", "<c-w><c-k>")
 map("n", "<c-l>", "<c-w><c-l>")
 map("n", "<c-h>", "<c-w><c-h>")
 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("v", "J", ":m '>+1<CR>gv=gv")
 
 vim.api.nvim_create_autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
