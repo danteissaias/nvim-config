442516ed3e895a82f5b273503e249d27fda917cc
Dante Issaias <dante@issaias.com>
2025-08-10 16:27:37 +0100

    Change fff shortcut

--- lua/plugins/fff-nvim.lua
+++ lua/plugins/fff-nvim.lua
@@ -1,14 +1,14 @@
 return {
   "dmtrKovalenko/fff.nvim",
   build = 'nix-shell -p openssl pkg-config --run "cargo build --release"',
-  -- keys = {
-  --   {
-  --     "<leader>sf",
-  --     function()
-  --       require("fff").toggle()
-  --     end,
-  --     desc = "Toggle FFF",
-  --   },
-  -- },
+  keys = {
+    {
+      "ff",
+      function()
+        require("fff").find_files()
+      end,
+      desc = "Toggle FFF",
+    },
+  },
   opts = {},
 }
