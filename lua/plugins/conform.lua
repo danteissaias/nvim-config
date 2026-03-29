2d35239b5c5c61c3a9934d08cdcd49b5d3189bc8
Dante Issaias <dante@issaias.com>
2026-03-29 16:15:48 +0100

    No more prettier or biome

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,52 +1,24 @@
-local get_js_formatter = function()
-  local cwd = vim.fn.getcwd()
-  local has_biome = vim.fn.filereadable(cwd .. "/biome.json") == 1
-
-  if has_biome then
-    return { "biome-check" }
-  else
-    return { "prettierd" }
-  end
-end
-
 return {
   "stevearc/conform.nvim",
   event = "BufWritePre",
   opts = {
     formatters_by_ft = {
-      javascript = get_js_formatter,
-      javascriptreact = get_js_formatter,
-      json = get_js_formatter,
-      jsonc = get_js_formatter,
       lua = { "stylua" },
       nix = { "nixfmt" },
-      typescript = get_js_formatter,
-      typescriptreact = get_js_formatter,
-      yaml = get_js_formatter,
       go = { "gofmt" },
     },
     format_on_save = function()
-      local cwd = vim.fn.getcwd()
-      local has_oxfmt = vim.fn.filereadable(cwd .. "/.oxfmtrc.json") == 1
-
       -- Stop if we disabled auto-formatting.
       if not vim.g.autoformat then
         return nil
       end
 
-      if has_oxfmt then
-        return {
-          timeout_ms = 500,
-          lsp_format = "prefer",
-          filter = function(client)
-            return client.name == "oxfmt"
-          end,
-        }
-      end
-
       return {
         timeout_ms = 500,
-        lsp_format = "fallback",
+        lsp_format = "prefer",
+        filter = function(client)
+          return client.name == "oxfmt"
+        end,
       }
     end,
   },
