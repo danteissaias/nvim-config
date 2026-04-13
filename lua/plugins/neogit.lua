8dbda876039570dccf7a79c6a2f4e9803a1fb410
Dante Issaias <dante@issaias.com>
2026-04-13 12:42:59 +0100

    neogit: Switch to delta pager

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,10 +3,13 @@ return {
   dependencies = {
     { src = "nvim-lua/plenary.nvim" },
     { src = "sindrets/diffview.nvim" },
+    { src = "m00qek/baleia.nvim" },
   },
   config = function()
+    vim.g.baleia = require("baleia").setup({})
     require("neogit").setup {
       graph_style = "kitty",
+      log_pager = { "delta", "--width", "117", "--file-style", "omit", "--hunk-header-style", "omit", "--keep-plus-minus-markers", "--paging", "never" },
     }
     vim.keymap.set("n", "<Leader>ng", ":Neogit<CR>", { silent = true })
   end,
